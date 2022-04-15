73a5ee7ad7464b806487ea33ea9871cb536ae1fa
FatemehSalimi-777 <fsalimi777@gmail.com>
2022-04-15 15:05:42 +0430

    last code

--- cw1.js
+++ cw1.js
@@ -1,5 +1,5 @@
  function lastElement(array, n) {
-    if (array == null) return void 0;
+    if (array == null) return  0;
     if (n == null) return array[array.length - 1];
     return array.slice(Math.max(array.length - n, 0));
   };
@@ -7,12 +7,6 @@
   console.log(lastElement([7, 9, 0, -2]));
   console.log(lastElement([7, 9, 0, -2], 3));
   console.log(lastElement([7, 9, 0, -2], 6));
-
-
-
-
-
-
 
 
 
