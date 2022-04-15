bbd524df241c4d1cba8b8ca6238319320c377fef
FatemehSalimi-777 <fsalimi777@gmail.com>
2022-04-15 16:56:39 +0430

    last changes

--- cw4.js
+++ cw4.js
@@ -1,14 +1,24 @@
+// function ConcatenatingUnevenArrays(arr) {
+//     arr.map(function (item, index) {
+
+//         console.log(`" ${arr[index]}" \n`);
+//     })
+// }
+
 function ConcatenatingUnevenArrays(arr) {
     arr.map(function (item, index) {
-
-        console.log(`" ${arr[index]}" \n`);
+         const newItem = item.reduce(function (acc, i) {
+            acc += " " + i;
+            return acc;
+        })
+        console.log(newItem);
     })
 }
-    ConcatenatingUnevenArrays([
-        ["The", "red", "horse"],
-        ["Plane", "over", "the", "ocean"],
-        ["Chocolate", "ice", "cream", "is", "awesome"],
-        ["this", "is", "a", "long", "sentence"]]);
+ConcatenatingUnevenArrays([
+    ["The", "red", "horse"],
+    ["Plane", "over", "the", "ocean"],
+    ["Chocolate", "ice", "cream", "is", "awesome"],
+    ["this", "is", "a", "long", "sentence"]]);
 
 // Write a Program to Concatenating uneven arrays : (use reduce )
 // example :
