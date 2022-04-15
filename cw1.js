4ca26fe173846392e3637318951b7f3b8fe96dcd
FatemehSalimi-777 <fsalimi777@gmail.com>
2022-04-15 14:25:46 +0430

    first cw

---
+++ cw1.js
@@ -0,0 +1,24 @@
+function lastElement(arr,n) {
+    let newArray = arr.slice(arr.length - 1);
+    return newArray;  
+}
+console.log(lastElement([7, 9, 0, -2]));
+
+
+
+
+
+
+
+
+
+// Write a JavaScript function to get the last element of an array. 
+//Passing a parameter 'n' will return the last 'n' elements of the array.
+// Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2],3));
+// console.log(last([7, 9, 0, -2],6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
